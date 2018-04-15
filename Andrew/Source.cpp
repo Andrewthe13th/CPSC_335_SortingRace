8c73a3ccc33bc62e96791c0e7afc07ab077d6452
Unknown <PaulSmith.Contactinfo@gmail.com>
2018-04-15 15:43:07 -0700

    Auto stash before merge of "master" and "origin/master"
    Added print for no numbers listed and added makefile and TestRuns.txt

--- Andrew/Source.cpp
+++ Andrew/Source.cpp
@@ -6,34 +6,38 @@
 
 int main(int argc, char *argv[])
 {
-	string arr1;
-    for (int i = 1; i < argc; i++){
-        arr1 += argv[ i ];
-        arr1 += " ";
-    }
-	stringstream iss( arr1 );
-	int number;
-	vector<int> list;
-	while (iss >> number ){
-		list.push_back( number );
-	}
+    if (argc != 1){
+	    string arr1;
+        for (int i = 1; i < argc; i++){
+            arr1 += argv[ i ];
+            arr1 += " ";
+        }
+	    stringstream iss( arr1 );
+	    int number;
+	    vector<int> list;
+	    while (iss >> number ){
+		    list.push_back( number );
+	    }
 
-	RaceMgr start (list);
-	cout << "Starting array is:[";
-	for (int i = 0; i < ( (static_cast<int>( list.size()))-1 ) ; i++)
-	{
-		cout << list[i] << ", ";
-	}
-	cout << list.back() << "]\n" << endl;
+	    RaceMgr start (list);
+	    cout << "Starting array is:[";
+	    for (int i = 0; i < ( (static_cast<int>( list.size()))-1 ) ; i++)
+	    {
+		    cout << list[i] << ", ";
+	    }
+	    cout << list.back() << "]\n" << endl;
 	
-	// adds check for a single element
-	if (static_cast<int>(list.size()) != 1)
-	{
-		start.run();
-		start.printWinner();
+	    // adds check for a single element
+	    if (static_cast<int>(list.size()) != 1)
+	    {
+		    start.run();
+		    start.printWinner();
+	    }
+	    else
+		    cout << "List is already sorted since it contains only one element." << endl;
 	}
 	else
-		cout << "List is already sorted since it contains only one element!!!\n";
+	    cout << "Please provide numbers to sort" << endl;
 
 	return 0;
 }
